c7cb1666b6cc0bee8056ea9026e6890b806d169a
Marwane <Marwane@192.168.1.39>
2020-11-21 19:36:21 +0100

    Heritage

---
+++ Heritage/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		
+		Voiture voiture = new Voiture ("BMW" , 10000) ;
+		
+	    voiture.accélerer();
+	    voiture.arreter();
+	    
+	    voiture.setMarque("Mercedes");
+	    System.out.println(voiture.getMarque());
+		
+
+	}
+
+}
